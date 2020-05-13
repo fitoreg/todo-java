2f8a1c48d44591f4cbb7b7692db4f3b6965cdfcc
Fitore Gashi <gashi.fitore@gmail.com>
2020-05-13 12:27:33 +0200

    Added TodoItem model class

---
+++ Todo/src/main/java/com/gashi/fitore/Todo/model/TodoItem.java
@@ -0,0 +1,56 @@
+package com.gashi.fitore.Todo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class TodoItem {
+
+    // Properties
+    @Id // Primary key
+    @GeneratedValue // Generates keys automatically
+    private Long id;
+    @NotBlank
+    private String title;
+    private boolean  isDone;
+
+
+    // Empty Constructor
+    public TodoItem() {
+    }
+
+    // Constructor
+    public TodoItem(Long id, String title, boolean isDone) {
+
+        this.id = id;
+        this.title = title;
+        this.isDone = isDone;
+    }
+
+    // Getters & Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+}
