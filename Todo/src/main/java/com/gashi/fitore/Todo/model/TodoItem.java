d4d88be36d69b025e35a573b39a75d15e4fe8808
Fitore Gashi <gashi.fitore@gmail.com>
2020-05-23 13:40:51 +0200

    Remove unused category property

--- Todo/src/main/java/com/gashi/fitore/Todo/model/TodoItem.java
+++ Todo/src/main/java/com/gashi/fitore/Todo/model/TodoItem.java
@@ -21,9 +21,6 @@ public class TodoItem {
     // Completion flag
     private boolean  isDone;
 
-    // Category of the Item
-    private TodoCategory category;
-
     // Empty Constructor
     public TodoItem() {
     }
@@ -58,13 +55,5 @@ public class TodoItem {
 
     public void setIsDone(boolean done) {
         isDone = done;
-    }
-
-    public TodoCategory getCategory() {
-        return category;
-    }
-
-    public void setCategory(TodoCategory category) {
-        this.category = category;
     }
 }
